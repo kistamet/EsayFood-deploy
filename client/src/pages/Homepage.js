26708da1b634b245b21e5484a9479a8a9124a790
kistamet <s6101012610011@email.kmutnb.ac.th>
2023-01-16 17:56:42 +0700

    Add categories

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -9,6 +9,24 @@ import { useCallback } from 'react';
 
 function Homepage() {
   const [itemsData, setItemsData] = useState([]);
+  const [selectedCategory, setSelectedCategoty] = useState("อาหารจานเดียว");
+  const categories = [
+    {
+      name: "อาหารจานเดียว",
+      imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2f/Culinary_fruits_front_view.jpg",
+    },
+    {
+      name: "ประเภททอด",
+      imageURL:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shopping-bag-full-of-fresh-vegetables-and-fruits-royalty-free-image-1128687123-1564523576.jpg",
+    },
+    {
+      name: "ประเภทต้ม",
+      imageURL:
+        "https://images.ctfassets.net/3s5io6mnxfqz/5GlOYuzg0nApcehTPlbJMy/140abddf0f3f93fa16568f4d035cd5e6/AdobeStock_175165460.jpeg?fm=jpg&w=900&fl=progressive",
+    },
+  ];
   const dispatch = useDispatch();
   const getAllItems = useCallback(() => {
     dispatch({ type: "showLoading" });
@@ -30,23 +48,34 @@ function Homepage() {
 
   // rest of the component
 
-    return (
-        <DefaultLayout>
+  return (
+    <DefaultLayout>
+      <div className="d-flex categories">
+        {categories.map((category) => {
+          return <div
+            onClick={() => setSelectedCategoty(category.name)}
+            className={`d-flex category ${selectedCategory === category.name && 'selected-category'}`}>
+            <h4>{category.name}</h4>
+            <img  src={category.imageURL} height='60' width='80' alt=""  />
+          </div>
+        })}
+      </div>
+      <Row gutter={20}>
+        {itemsData.filter((i) => i.category===selectedCategory).map((item) => {
+          return <Col span={6} xs={24} lg={6} md={12} sm={6}>
 
-            <Row gutter={20}>
-                {itemsData.map((item) => {
-                    return <Col span={6} xs={24} lg={6} md={12} sm={6}>
+            <Item item={item} />
 
-                        <Item item={item} />
+          </Col>
 
-                    </Col>
-
-                })}
-            </Row>
+        })}
+      </Row>
 
 
-        </DefaultLayout>
-    )
+    </DefaultLayout>
+  )
 }
+
+
 
 export default Homepage
